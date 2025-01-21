170ebdea45b557d07ac88d39deb5784772ebbf59
Quentin Legiemble <q.legiemble@gmail.com>
2025-01-21 11:48:11 +0100

    mongo comment

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ app.use(bodyParser.json());
 const mongoose = require('mongoose');
 const messageEmitter = require('./event');
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Error connecting to MongoDB:', err));
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log('Connected to MongoDB'))
+//     .catch(err => console.error('Error connecting to MongoDB:', err));
 
 app.use((req, res, next) => {
     log.rotateLog();
